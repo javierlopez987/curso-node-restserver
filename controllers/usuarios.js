97c9d0468062e1b14e8af4ce1cbbedc93a35299d
Javier Lopez Ferreira <javierlopez987@gmail.com>
2022-06-23 18:16:07 -0300

    obtener datos del body - HTTP POST

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,20 +1,23 @@
 const { response } = require('express');
 
 const usuariosGet = (req, res = response) => {
-  
   res.json({
     msg: 'HTTP GET API controller'
   });
 };
 
 const usuariosPost = (req, res = response) => {
+  const { nombre, edad } = req.body;
+
   res.status(201).json({
-    msg: 'HTTP POST API controller'
+    msg: 'HTTP POST API controller',
+    nombre,
+    edad
   });
 };
 
 const usuariosPut = (req, res) => {
-  res.status(500).json({
+  res.json({
     msg: 'HTTP PUT API controller'
   });
 };
