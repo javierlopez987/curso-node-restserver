32f526975a61952360be49e09fcabed4df02e536
Javier Lopez Ferreira <javierlopez987@gmail.com>
2022-06-23 17:14:43 -0300

    http get, put, post, delete

--- models/server.js
+++ models/server.js
@@ -19,8 +19,32 @@ class Server {
   }
 
   routes() {
+    // HTTP GET
     this.app.get('/api', (req, res) => {
-      res.send('Hello World!');
+      res.json({
+        msg: 'HTTP GET API call'
+      });
+    });
+
+    // HTTP PUT
+    this.app.put('/api', (req, res) => {
+      res.json({
+        msg: 'HTTP PUT API call'
+      });
+    });
+
+    // HTTP POST
+    this.app.post('/api', (req, res) => {
+      res.json({
+        msg: 'HTTP POST API call'
+      });
+    });
+
+    // HTTP DELETE
+    this.app.delete('/api', (req, res) => {
+      res.json({
+        msg: 'HTTP DELETE API call'
+      });
     });
   }
 
